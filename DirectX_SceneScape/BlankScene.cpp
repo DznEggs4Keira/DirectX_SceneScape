c50ed3dfece687a0dbb78fdf33e61d2baa36fe55
Zainab Ali <47698736+DznEggs4Keira@users.noreply.github.com>
2021-03-27 12:20:40 +0400

    updated the git ignore file

--- DirectX_SceneScape/BlankScene.cpp
+++ DirectX_SceneScape/BlankScene.cpp
@@ -1,3 +1,8 @@
+///////////////////////////////////////////////////////////////////////////
+//// This Class will become the Scene Manager Class which will handle ////
+//// which Scene will be loaded and when.                            ////
+////////////////////////////////////////////////////////////////////////
+
 #include "BlankScene.h"
 
 BlankScene::BlankScene()
@@ -31,7 +36,7 @@ void BlankScene::Render()
         return;
 
     // Clear back buffer
-    float color[4] = { 0.1f, 0.2f, 0.5f, 1.0f };
+    float color[4] = { 0.5f, 0.0f, 0.5f, 1.0f };
     m_pD3DContext->ClearRenderTargetView(m_pD3DRenderTargetView, color);
 
     // Present back buffer to display
